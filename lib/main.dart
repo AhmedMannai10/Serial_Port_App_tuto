081e7426fd8442e9567040e7229e878378475532
DrGhost <mannaiahmed7518@gmail.com>
2023-10-28 19:44:29 +0100

    updating the README file

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               borderSide: BorderSide.none,
             ),
           )),       
-          home: HomePage()
+          home: const HomePage()
     );
   }
 }
